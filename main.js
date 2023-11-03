67c982b2fbfc7f87fa7d5c6ca7535885333d7b82
this.Konrad <konradlebt@gmail.com>
2023-11-03 17:00:43 +0100

    write the function

--- main.js
+++ main.js
@@ -1,4 +1,54 @@
 
-let imageSource = document.querySelectorAll('image').src;
+const images = document.querySelectorAll('img');
 
-console.log(imageSource);
+/** array for the url collection:  */
+let imageSourceArray = [];
+
+/** push every url in an array: */
+images.forEach( image => {
+
+    let imageSource = image.getAttribute('src');
+
+    imageSourceArray.push(imageSource);
+
+});
+
+console.log(imageSourceArray);
+
+/* declare a variable for the random number of the url":*/
+let imageNumber;
+
+for( let i = 0; i < imageSourceArray.length; i++){
+
+    /* get the url */
+    let sourceUrl = imageSourceArray[i];
+
+    console.log(sourceUrl);
+
+
+    /* loop over the urls and get the last letter */
+    for( let letter of sourceUrl){
+
+        console.log('letter: ',letter.length);
+
+        if( letter.length === 38){
+
+            imageNumber = letter.charAt(-1);
+
+            imageNumber = `${randomSingle}`;
+            
+        }
+        if( letter.length === 39){
+            
+            imageNumber = letter.charAt(-2);
+
+            imageNumber = `${randomDouble}`;
+        }
+
+    }
+
+};
+
+
+
+
