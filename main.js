94e8bb64094da1dce86705329f62118062df60ad
this.Konrad <konradlebt@gmail.com>
2023-11-03 18:32:43 +0100

    css class with animation adde in js to image

--- main.js
+++ main.js
@@ -2,18 +2,18 @@
 const images = document.querySelectorAll('img');
 
 
-
-function changeUrlEnding(){
+function changeUrlEndings(){
 
     images.forEach( image => {
 
+        /* get the url and change it for iteration into an array: */
         let imageSource = image.getAttribute('src');
     
         let urlArray = Array.from(imageSource);
     
         console.log("ArrayLEngth: ", urlArray.length);
     
-    
+        /* replace the default random number of the image url: */
         for( let i = 0; i < urlArray.length; i ++){
     
             let randomNumber = Math.floor(Math.random() * ( 100 - 1) + 1);
@@ -23,10 +23,17 @@ function changeUrlEnding(){
     
         }
     
+        /* turn it into strings again: */
         let urlArrayToString = urlArray.join('')
         console.log("randomArrays: ",urlArrayToString );
+
+        /* replace the image src: */
+        image.setAttribute("src", urlArrayToString);
+
+        image.classList.add('image_change');
     
     });
     
-
 };
+
+setInterval(()=>{ changeUrlEndings() },(4000));
